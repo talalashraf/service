21d982cb89e6067549ea449beb62ca0782d50144
Steven Soroka <ssoroka@surfeasy.com>
2015-05-12 15:54:48 -0400

    add quit variable

--- service.go
+++ service.go
@@ -1,87 +1,87 @@
 package service
 
 import (
-  "os"
-  "os/signal"
-  "sync"
-  "time"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
 )
 
 type Service struct {
-  wg          sync.WaitGroup
-  quitChannel chan bool
+	wg          sync.WaitGroup
+	quitChannel chan bool
 }
 
 // use once per application
 func NewService() *Service {
-  s := Service{
-    quitChannel: make(chan bool, 0),
-    wg:          sync.WaitGroup{},
-  }
-  go s.processSignals()
-  return &s
+	s := Service{
+		quitChannel: make(chan bool, 0),
+		wg:          sync.WaitGroup{},
+	}
+	go s.processSignals()
+	return &s
 }
 
 func (s *Service) processSignals() {
-  sigChan := make(chan os.Signal, 0)
-  signal.Notify(sigChan, os.Interrupt, os.Kill)
-  <-sigChan
-  close(s.quitChannel)
+	sigChan := make(chan os.Signal, 0)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+	<-sigChan
+	close(s.quitChannel)
 }
 
-func (s *Service) Timer(duration time.Duration, f func()) {
-  t := time.NewTicker(duration)
-  quit := false
-  for !quit {
-    select {
-    case <-s.quitChannel:
-      t.Stop()
-      quit = true
-    case <-t.C:
-      f()
-    }
-  }
-  s.wg.Done()
+func (s *Service) Timer(duration time.Duration, f func(quit *bool)) {
+	t := time.NewTicker(duration)
+	quit := false
+	for !quit {
+		select {
+		case <-s.quitChannel:
+			t.Stop()
+			quit = true
+		case <-t.C:
+			f(&quit)
+		}
+	}
+	s.wg.Done()
 }
 
 func (s *Service) ChannelReader(c chan interface{}, f func(*interface{})) {
-  var data interface{}
-  quit := false
-  for !quit {
-    select {
-    case <-s.quitChannel:
-      quit = true
-    case data = <- c:
-      f(&data)
-    }
-  }
-  s.wg.Done()
+	var data interface{}
+	quit := false
+	for !quit {
+		select {
+		case <-s.quitChannel:
+			quit = true
+		case data = <-c:
+			f(&data)
+		}
+	}
+	s.wg.Done()
 }
 
 func (s *Service) Loop(f func()) {
-  quit := false
-  for !quit {
-    select {
-    case <-s.quitChannel:
-      quit = true
-    default:
-      f()
-    }
-  }
-  s.wg.Done()
+	quit := false
+	for !quit {
+		select {
+		case <-s.quitChannel:
+			quit = true
+		default:
+			f()
+		}
+	}
+	s.wg.Done()
 }
 
 func (s *Service) StartLoop(f func()) {
-  s.Start(func() {
-    s.Loop(f)
-  })
+	s.Start(func() {
+		s.Loop(f)
+	})
 }
 
 func (s *Service) Start(f func()) {
-  s.wg.Add(1)
-  go f()
+	s.wg.Add(1)
+	go f()
 }
 
 func (s *Service) Wait() {
-  s.wg.Wait()
+	s.wg.Wait()
 }
